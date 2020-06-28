54117db3d618010c901ce9874f5b81b473a6d02e
Muhammad Ridjal Fathoni Rosyidi <fathonirosyidi@gmail.com>
2020-06-28 23:44:51 +0800

    navbar and footer

---
+++ src/components/footer.component.vue
@@ -0,0 +1,238 @@
+<template>
+    <div id="fot">
+        <div class="container">
+            <div class="footer-info">
+                <div class="row">
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>HUBUNGI KAMI</h6>
+                        </div>
+                        <div class="footer-contact">
+                            <p>
+                                0274-5305505 <br>
+                                Senin - Minggu <br>
+                                24 Jam Nonstop 
+                            </p>
+                        </div>
+                        <div class="footer-address">
+                            <p>
+                                Jl. Selokan Mataram Monjali <br>
+                                Karangjati MT I/304 <br>
+                                Sinduadi, Mlati, Sleman <br>
+                                Yogyakarta 55284
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>LAYANAN</h6>
+                        </div>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="#">Domain</a>
+                            </li>
+                            <li>
+                                <a href="#">Shared Hosting</a>
+                            </li>
+                            <li>
+                                <a href="#">Cloud VPS Hosting</a>
+                            </li>
+                            <li>
+                                <a href="#">Managed VPS Hosting</a>
+                            </li>
+                            <li>
+                                <a href="#">Web Builder</a>
+                            </li>
+                            <li>
+                                <a href="#">Keamanan SSL/HTTPS</a>
+                            </li>
+                            <li>
+                                <a href="#">Jasa Pembuatan Website</a>
+                            </li>
+                            <li>
+                                <a href="#">Program Afiliasi</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>SERVICE HOSTING</h6>
+                        </div>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="#">Hosting Murah</a>
+                            </li>
+                            <li>
+                                <a href="#">Hosting Indonesia</a>
+                            </li>
+                            <li>
+                                <a href="#">Hosting Singapura SG</a>
+                            </li>
+                            <li>
+                                <a href="#">Hosting PHP</a>
+                            </li>
+                            <li>
+                                <a href="#">Hosting Wordpress</a>
+                            </li>
+                            <li>
+                                <a href="#">Hosting Laravel</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>TUTORIAL</h6>
+                        </div>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="#">Knowledgebase</a>
+                            </li>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Cara Pembayaran</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>TENTANG KAMI</h6>
+                        </div>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="#">Tim Hostinger</a>
+                            </li>
+                            <li>
+                                <a href="#">Karir</a>
+                            </li>
+                            <li>
+                                <a href="#">Events</a>
+                            </li>
+                            <li>
+                                <a href="#">Penawaran & Promo Spesial</a>
+                            </li>
+                            <li>
+                                <a href="#">Kontak Kami</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>KENAPA PILIH NIAGAHOSTER?</h6>
+                        </div>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="#">Support Terbaik</a>
+                            </li>
+                            <li>
+                                <a href="#">Garansi Harga Termurah</a>
+                            </li>
+                            <li>
+                                <a href="#">Domain Gratis Selamanya</a>
+                            </li>
+                            <li>
+                                <a href="#">Datacenter Hosting Terbaik</a>
+                            </li>
+                            <li>
+                                <a href="#">Review Pelanggan</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <div class="footer-header">
+                            <h6>NEWSLETTER</h6>
+                        </div>
+                        <form id="newsletter" action="" class="footer-newsletter d-flex flex-row">
+                            <input type="email" placeholder="Email" name="email" class="input-newsletter form-control">
+                            <button class="btn btn-newsletter" type="submit">Berlangganan</button>
+                        </form>
+                        <p>
+                            Dapatkan promo dan konten menarik <br>
+                            dari penyedia hosting terbaik Anda.
+                        </p>
+                    </div>
+                    <div class="col-12 col-sm-3 mb-5">
+                        <br>
+                        <div class="footer-social-media d-flex flex-row">
+                            <a class="footer-facebook" href="#">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a class="footer-twitter" href="#">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a class="footer-google" href="#">
+                                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12">
+                        <div class="footer-header">
+                            <h6>Pembayaran</h6>
+                        </div>
+                        <div class="payment-content d-flex flex-column">
+                            <div class="payment-img d-flex flex-row flex-wrap">
+                                <div class="payment-holder">
+                                    <img src="../assets/images/BCA.png" alt="BCA" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/mandiri.png" alt="Mandiri" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/BNI.png" alt="BNI" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/visa.png" alt="VISA" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="https://www.niagahoster.co.id/assets/images/2019/payment-gateway/master.svg" alt="MasterCard" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/ATM-Bersama.png" alt="ATM Bersama" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/logo permata bank.png" alt="PermataBank" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="../assets/images/Prima.png" alt="Prima" class="grayscale">
+                                </div>
+                                <div class="payment-holder">
+                                    <img src="https://www.niagahoster.co.id/assets/images/2019/payment-gateway/alto.svg" alt="Alto" class="grayscale">
+                                </div>
+                            </div>
+                            <div class="payment-text mt-3 mb-3">
+                                <p>Aktivasi instan dengan e-Payment. Hosting dan domain langsung aktif!</p>
+                            </div>
+                            <div class="footer-border-bottom"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright mt-3">
+                <div class="row">
+                    <div class="col-12 col-md-8">
+                        <p class="text-copyright">
+                            Copyright ©2016 Niagahoster | Hosting powered by PHP7, CloudLinux, CloudFlare, BitNinja and DC Biznet Technovillage Jakarta. Cloud VPS Murah powered by Webuzo Softaculous, Intel SSD and cloud computing technology
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-4 ">
+                        <div class="privacy-policy text-center text-md-right">
+                            <a href="#">Syarat dan Ketentuan</a>
+                            <a href="#">Kebijakan Privasi</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/nav.component.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="navigasi">
+        <section id="top-nav">
+            <div class="top-nav d-none d-sm-block">
+                <div class="container h-100 d-flex flex-row flex-wrap align-items-center">
+                    <div class="ebook-container desktop" v-if="isShow">
+                        <div class="d-flex align-items-center d-none d-lg-block d-sm-none">
+                            <div class="badges">
+                                <i class="fa fa-tag" aria-hidden="true"></i>
+                            </div>
+                            <a href="#">Gratis Ebook 9 Cara Cerdas Menggunakan Domain </a><span class="close-badge" @click="btnClose">[x]</span>
+                        </div>
+                    </div>
+                    <div class="info-container ml-md-auto ml-lg-auto ml-0 mx-lg-0 mx-md-auto mx-sm-auto">
+                        <div class="d-flex justify-content-center align-items-center flex-row flex-wrap">
+                            <div class="info-item">
+                                <i class="fa fa-phone mr-2" aria-hidden="true"></i>
+                                <span>0274-5305505</span>
+                            </div>
+                            <div class="info-item">
+                                <i class="fa fa-comments mr-2" aria-hidden="true"></i>
+                                <span>Live Chat</span>
+                            </div>
+                            <div class="info-item">
+                                <i class="fa fa-user-circle mr-2" aria-hidden="true"></i>
+                                <span>Member Area</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="navbar">
+            <nav class="navbar navbar-expand-sm navbar-light">
+                <div class="container">
+                    <div class="navbar-logo navbar-brand d-lg-inline-block d-md-none d-sm-inline-block">
+                        <img src="../assets/images/logo-niagahoster.jpg" alt="">
+                    </div>
+                    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </button>
+                    <div class="collapse navbar-collapse mx-sm-auto" id="collapsibleNavId">
+                        <ul class="navbar-nav mx-sm-auto mx-md-auto mx-lg-0 mt-2 mt-lg-0 ml-md-auto ml-lg-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Hosting</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Domain</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Server</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Website</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Afiliasi</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Promo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pembayaran</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Review</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Kontak</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'navigation',
+    data() {
+        return {
+            isShow: true
+        }
+    },
+    methods: {
+        btnClose() {
+            this.isShow = false;
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.top-nav {
+    height: 40px;
+    border-bottom: 1px solid #c4c4c4;
+    color: #4C4C4C;
+    /* padding: 0px 20px; */
+    .ebook-container a {
+        text-decoration: none;
+        color: #333;
+        position: relative;
+        margin-left: 3rem;
+    }
+
+    .badges {
+        clip-path: polygon(50% 70%, 100% 100%, 100% 0, 0 0, 0 100%);
+        background: #008FEE;
+        width: 30px;
+        height: 30px;
+        position: absolute;
+        top: 0;
+
+        * {
+            bottom: 65%;
+            right: 65%;
+            position: absolute;
+            transform: translateX(50%) translateY(25%);
+            font-size: 12px;
+            color: white;
+        }
+    }
+
+    .close-badge {
+        cursor: pointer;
+    }
+}
+
+#navbar {
+    .navbar-logo {
+        height: 100%;
+        width: 20%;
+        padding: 10px;
+
+        img {
+            height: 100%;
+            width: 100%;
+        }
+    }
+}
+
+@media (max-width: 768px) {
+  #navbar {
+    .navbar-logo {
+        height: 100%;
+        width: 50%;
+        padding: 10px;
+
+        img {
+            height: 100%;
+            width: 100%;
+        }
+    }
+}
+}
+</style>
