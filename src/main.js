73dfd35839cfff3e0fb0626a39a96b496afc4c76
Muhammad Ridjal Fathoni Rosyidi <fathonirosyidi@gmail.com>
2020-06-28 23:45:09 +0800

    lib

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/style.css'
+import './assets/css/font-familly.css'
+
+import jQuery from 'jquery'
+global.jQuery = jQuery
+global.$ = jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+let Bootstrap = require('bootstrap')
+
+import Meta from "vue-meta";
+Vue.use(Meta);
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
