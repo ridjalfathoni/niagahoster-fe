0c28aac221f359e1d7d650e2596e719cf95985dc
Muhammad Ridjal Fathoni Rosyidi <fathonirosyidi@gmail.com>
2020-06-29 06:42:09 +0800

    get data from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import types from './types'
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    package: window.localStorage.getItem('package') || '',
   },
   mutations: {
+    [types.GET_PACKAGE](state) {
+      axios.get('http://localhost:8000/api/packet').then((response) => {
+          localStorage.setItem('package', JSON.stringify(response.data))
+      })
+    }
   },
   actions: {
+    [types.GET_PACKAGE](context) {
+      context.commit(types.GET_PACKAGE)
+    }
   },
   modules: {
   }
