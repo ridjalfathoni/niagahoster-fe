1464e5a616df5e04277360d6268d8582bbcc927b
Muhammad Ridjal Fathoni Rosyidi <fathonirosyidi@gmail.com>
2020-06-28 23:47:21 +0800

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="border">
+      <navbar></navbar>
     </div>
     <router-view/>
+    <div id="foot">
+      <footers></footers>
+    </div>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import navbar from '../src/components/nav.component'
+import footers from '../src/components/footer.component'
+export default {
+  components: {
+    navbar,
+    footers
+  },
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
